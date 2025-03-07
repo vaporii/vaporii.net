8d96a21be0272e04cd7eb3c1292bbf55f9419b09
vaporii <phi548182@gmail.com>
2025-03-07 09:52:43 -0600

    layout

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
-	"html/template"
+	// "html/template"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-	})
+	// });
+
+	fs := http.FileServer(http.Dir("./public"))
+
+	http.Handle("/", http.StripPrefix("/", fs))
+
+	port := ":8080"
+	log.Println("serving on http://localhost" + port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
