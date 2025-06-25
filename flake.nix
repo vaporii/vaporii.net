e72d6d917a55a023dac4b57d4467e3f33abf525c
Ellie Phipps <phi548182@gmail.com>
2025-06-25 12:59:11 -0500

    remove module stuff

--- flake.nix
+++ flake.nix
@@ -12,9 +12,6 @@
   in {
     packages.${system}.default = (import ./nix { inherit pkgs; });
 
-    nixosModules.default = self.nixosModules.vaporii;
-    nixosModules.vaporii = import ./nix/nixos.nix inputs;
-
     devShells.x86_64-linux.default = pkgs.mkShell {
       packages = with pkgs; [
         go
