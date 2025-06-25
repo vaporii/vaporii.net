aa47376ed495fab59dbbc1d7d5ef065da6fa282b
Ellie Phipps <phi548182@gmail.com>
2025-06-25 11:54:07 -0500

    fix nix

--- nix/default.nix
+++ nix/default.nix
@@ -5,28 +5,20 @@
 
     src = ../.;
 
-    vendorHash = "sha256-AAAKAJHNEFJSB";
-
-    # installPhase = ''
-    #   mkdir -p $out/bin/v8p.me
-    #   cp -r build $out/bin/v8p.me
-    #   cp -r node_modules $out/bin/v8p.me
-    # '';
+    vendorHash = "sha256-iAqEgyzxDddu3o9DaZx58tEw00dVgAXb47ZIcFnms6Y=";
   };
 in
 pkgs.dockerTools.buildImage {
   name = "vaporii.net";
   tag = "latest";
 
-  config = {
-    Cmd = [ "${vaporiiPkg}/bin/vaporii.net" ];
+  copyToRoot = pkgs.buildEnv {
+    name = "image-root";
+    paths = [ vaporiiPkg ];
+    pathsToLink = [ "/bin" ];
   };
 
-#   copyToRoot = pkgs.buildEnv {
-#     name = "v8p.me";
-#     paths = [ v8pPkg pkgs.coreutils pkgs.nodejs_22 ];
-#     pathsToLink = [ "/bin" "/lib" ];
-#   };
-
-#   keepContentsDirlinks = true;
+  config = {
+    Cmd = [ "/bin/server" ];
+  };
 }
